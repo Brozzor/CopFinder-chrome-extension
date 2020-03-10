a41fff8666f22290c7b6509b07b330cafc7684af
Brozzor <46285168+Brozzor@users.noreply.github.com>
2020-03-10 18:53:22 +0100

    feat (fo) ajout de la connection ajax entre l'extension l'api php

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function load(event){
-    let createButton = document.getElementById('lunch');
-    createButton.addEventListener('click', function() { lunch(); });
+    loginPage();
+    let createButton = document.getElementById('launch');
+    createButton.addEventListener('click', function() { launch(); return false;});
 
     let createButton2 = document.getElementById('buyLicence');
     createButton2.addEventListener('click', function() { redirectToBuy(); });
@@ -9,21 +10,34 @@ window.addEventListener('load', function load(event){
 function loginPage(){
 document.getElementById('app').innerHTML = `
 <div class="login-page">
-        <div class="form card">
-          <form class="input-form login-page">
-            <input type="mail" placeholder="Mail address"/>
-            <input type="token" placeholder="Token key"/>
-            <button onclick="lunch()" class="btn">Lunch app</button>
-            <p class="message">Not token key ? <a onclick="lunch()" href="#">Buy licence</a></p>
-          </form>
+      <br />
+      <div class="form card">
+      <center>
+        <img width="200" src="img/logo.png" alt="" />
+      </center>
+        <div class="input-form login-page">
+          <input id="mail" type="mail" placeholder="Mail address" />
+          <input id="token" type="token" placeholder="Token key" />
+          <button id="launch" class="btn">Launch app</button>
+          <p class="message">No token key ? <a id="buyLicence" href="#">Buy licence</a></p>
         </div>
-      </div>`;
+      </div>
+    </div>
+    `;
 }
 
 function redirectToBuy(){
-    chrome.tabs.create({url : 'http://cop-finder.com'});
+    chrome.tabs.create({url : 'http://cop-finder.com/'});
 }
 
-function lunch(){
-    alert('cc');
+function launch(){
+  let xhr = new XMLHttpRequest();
+  let mail = document.getElementById('mail').value;
+  let token = document.getElementById('token').value;
+  let use = 'login';
+	xhr.open("POST", "http://cop-finder.com/api/login.php", true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send("mail="+ mail + "&token=" + token + "&use=" + use);
+
+    console.log(xhr);
 }
